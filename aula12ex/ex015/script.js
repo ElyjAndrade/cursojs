7869427ebc82704dd0e3e56a9ec4f89144fa0043
Ely Andrade <126113145+ElyjAndrade@users.noreply.github.com>
2023-05-10 19:29:43 -0300

    .

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -15,18 +15,40 @@ function verificar() {
         genero = 'Homem'
         if (idade >= 0 && idade < 12) {
         //criança
+        img.setAttribute('src', 'menino.jpg')
         } else if (idade < 21 ) {
             //jovem
+            img.setAttribute('src', 'adolescenth.jpg')
         } else if (idade < 50) {
             //adulto
+            img.setAttribute('src', 'homem.jpg')
         } else {
             //idoso
+            img.setAttribute('src', 'idoso.jpg')
         } 
         
             
        } else if (fsex[1].checked) {
         genero = 'Mulher'
+        if (idade >= 0 && idade < 12) {
+            //criança
+            img.setAttribute('src', 'menina.jpg')
+            } else if (idade < 21 ) {
+                //jovem
+                img.setAttribute('src', 'adolescentm.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'mulher.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosa.jpg')
+            } 
        }
+       res.style.textAlign = 'center'
        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+       res.appendChild(img).style.borderRadius = '50%'
+       res.appendChild(img).style.height = '200px'
+       res.appendChild(img).style.width = '250px'
+
     }
 }
