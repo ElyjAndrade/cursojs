46add9fd7cab4d0089680eab6daeeed1b4e0153a
Ely Andrade <126113145+ElyjAndrade@users.noreply.github.com>
2023-05-22 08:56:54 -0300

    .

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while( c <= 5)
+
+
+
+
+
+
+
+
+/*var c = 1
+while( c <= 4 ) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/ 
