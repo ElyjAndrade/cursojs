4ebed8613545fdce3c3358b46c5126a4634df034
Ely Andrade <126113145+ElyjAndrade@users.noreply.github.com>
2023-05-22 09:43:00 -0300

    .

---
+++ aula14/ambiente.js
@@ -0,0 +1,24 @@
+console.log('Vai começar....')
+for(var c = 1;c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+
+
+
+
+/* var c = 1
+while (c <=10 ) {
+    console.log(c) 
+    c++
+}           forma while */ 
+
+
+/* var c = 1
+do {
+    console.log(c) 
+    c++
+} while (c <=10 ) 
+
+Forma DO WHILE */
