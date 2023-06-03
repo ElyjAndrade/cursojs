a69c6ed55df2344a2a1fc140f5048d3a3dbc8108
Ely Andrade <126113145+ElyjAndrade@users.noreply.github.com>
2023-06-03 14:18:29 -0300

    .

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,8 +5,28 @@ function contar() {
     var res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert(`[ERRO] Faltam dados!`)
-    } else {
-        
+        res.innerHTML = 'Impossivel contar!'
+        } else {
+        res.innerHTML = `Contando: <br>`
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Invalido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //crescente
+            for(var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}` 
+            }
+            } else {
+                //decrescente
+                for(var c = i; c >= f; c -= p) {
+                  res.innerHTML += `${c} \u{1F449}`  
+                }
+               
+            }
+            res.innerHTML += `\u{1F3C1}`
     }
 }
