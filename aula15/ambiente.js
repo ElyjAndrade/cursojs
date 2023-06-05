cfa0f42047db56fc5e7585ac8dfca6a25bb9f280
Ely Andrade <126113145+ElyjAndrade@users.noreply.github.com>
2023-06-05 18:55:07 -0300

    .

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+var num = [5, 1, 7, 8, 9]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
---
+++ vetornatela.js
@@ -0,0 +1,11 @@
+var valores = [8, 1, 7, 4, 6, 7, 9]
+for(var pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+/*var valores = [8, 1, 7, 4, 6, 7, 9]
+for(var pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+console.log(valores[0])*/ 
