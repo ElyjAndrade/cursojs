b0bb19bdb5d19d5df6c26efb673d090418bee997
Ely Andrade <126113145+ElyjAndrade@users.noreply.github.com>
2023-06-13 21:33:52 -0300

    ..

--- ultimoexerpart7/script.js
+++ ultimoexerpart7/script.js
@@ -21,8 +21,27 @@ function inList(n, l) {
 
 function adicionar() {
     if(isNum(num.value) && !inList(num.value, valores)) {
-        window.alert('tudo ok chefia!')
+       valores.push(Number(num.value))  
+       var item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado.`
+       list.appendChild(item)
+       res.innerHTML = ''
+       
     } else {
         window.alert('O valor digitado é inválido ou já foi inserido na lista!')
+    }
+    num.value = ''
+    num.focus()
+    
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert(`Adicione valores antes de finalizar!`)
+    } else {
+        var tot = valores.length 
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Temos ${tot} números cadastrados.</p>`
     }
 }
